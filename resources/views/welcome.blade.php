7f187ed448d0f734d7c87211aa6929c0d4b8f21f
vagrant <vagrant@localhost.localdomain>
2018-10-26 10:45:59 +0000

    asjdka

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HEY
                 </div>
 
                 <div class="links">
