1e1418731129f6749c65416e6964bfe27cdf546f
John <makerareps@gmail.com>
2018-10-29 08:41:18 +0000

    Asdsada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    booasdasdasmbayaa
+                    12312312
                 </div>
 
                 <div class="links">
