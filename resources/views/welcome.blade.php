3e9dd4d42200ae38a35f0201eed496f561ff7231
John <jLkisni@yahoo.com>
2018-10-29 17:53:14 +0800

    Adding new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    sample 1
+                    sample 23
                 </div>
 
                 <div class="links">
