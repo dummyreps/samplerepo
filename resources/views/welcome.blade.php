44883f2d20a5243a7f31e30a061bb04d12c64c33
John <makerareps@gmail.com>
2018-10-29 08:37:24 +0000

    LAsdjkjasd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    boombayaa
+                    booasdasdasmbayaa
                 </div>
 
                 <div class="links">
