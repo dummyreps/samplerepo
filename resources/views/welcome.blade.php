f9f6702fac4a8313005c864df54ceecf77418702
John <jLkisni@yahoo.com>
2018-10-29 14:55:51 +0800

    Adding new web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    testing 2
+                    boombayaa
                 </div>
 
                 <div class="links">
