a994ff7e5937f61ba3ca020170e7398db7b97fa9
John <jLkisni@yahoo.com>
2018-10-29 18:13:23 +0800

    Adding new files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   halloooooooooooooooooooooooo
+                   qweqwasdasdsa
                 </div>
 
                 <div class="links">
