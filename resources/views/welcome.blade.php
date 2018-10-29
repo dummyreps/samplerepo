0e95fe8bf0db797336b9801881271d58097e95a1
John <jLkisni@yahoo.com>
2018-10-29 15:55:32 +0800

    Add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    boombayaa
+                    add
                 </div>
 
                 <div class="links">
