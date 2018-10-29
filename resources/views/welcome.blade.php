b2465af18bdafed7dc5def75cc11640de756e56d
John <makerareps@gmail.com>
2018-10-29 09:41:50 +0000

    lasjdlsajda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    asdqwewqasdas
+                    weewwew
                 </div>
 
                 <div class="links">
