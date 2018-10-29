394092487d85df3b74c3f92cc7e8d3fc87839a74
John <jLkisni@yahoo.com>
2018-10-29 18:05:36 +0800

    Adding halloo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ryo SANNNNN
+                   halloooooooooooooooooooooooo
                 </div>
 
                 <div class="links">
