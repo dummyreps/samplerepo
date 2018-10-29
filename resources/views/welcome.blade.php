29e979b3b0af4c68211a5e758cbc84378d6b33f1
John <jLkisni@yahoo.com>
2018-10-29 14:38:00 +0800

    Testing 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    heyouuuyyyyyyyy
+                    testing 2
                 </div>
 
                 <div class="links">
