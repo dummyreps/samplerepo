141be4477a5d51df7b1ef5afc760dcee075c1569
John <jLkisni@yahoo.com>
2018-10-30 16:36:11 +0800

    asdqwe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   TARONG BA
+                   watafakA
                 </div>
 
                 <div class="links">
