9ae6933b9758fad7a0da4fd5d5a0c1cc9b0bef64
John <jLkisni@yahoo.com>
2018-10-30 16:34:53 +0800

    asdasdsa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Wewwwwww
+                   TARONG BA
                 </div>
 
                 <div class="links">
