55559aee6928dac35895959bfda392d14bbaf214
John <jLkisni@yahoo.com>
2018-10-30 15:54:49 +0800

    Adding to master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   qweqwasdasdsa
+                   Ryo Ochinchin
                 </div>
 
                 <div class="links">
